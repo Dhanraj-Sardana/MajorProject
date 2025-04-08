a92548db3ddb9b09367e3a0df311993a4e166798
DHANRAJSARDANA <dhanrajsardana2003@gmail.com>
2025-04-08 14:59:03 +0530

    createbranch and switchbranch

--- front_end/electron/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electronAPI", {
-    runCommand: (command) => ipcRenderer.invoke("run-command", command),
-});
